7d3bd58a8fba3cf5a5e017fe9c9a835e4c019ef6
Shadkoech <shadrackipruto@gmail.com>
2024-03-24 19:55:01 +0300

    Selecting and removing elements

---
+++ Day_010/script2.js
@@ -0,0 +1,7 @@
+const body = document.body
+const div = document.querySelector("div")
+const spanHi = document.querySelector('#hi')
+const spanok = document.querySelector('#okay')
+
+// Removing an element
+spanHi.remove()
