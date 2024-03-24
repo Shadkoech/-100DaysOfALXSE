b7984c2caca5a1699450be1703e60dac08e579b8
Shadkoech <shadrackipruto@gmail.com>
2024-03-24 20:26:22 +0300

    Modifying Data Attributes

--- Day_010/script2.js
+++ Day_010/script2.js
@@ -5,3 +5,8 @@ const spanok = document.querySelector('#okay')
 
 // Removing an element
 spanHi.remove()
+console.log(spanHi.getAttribute("id"))
+console.log(spanHi.dataset.test)
+
+// add new class under a given div
+spanHi.classList.add("new-class")
