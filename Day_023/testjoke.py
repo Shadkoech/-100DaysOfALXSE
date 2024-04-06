e188d67ef6bc03d30c235b3a5276c9303965b41b
Shadkoech <shadrackipruto@gmail.com>
2024-04-06 13:53:16 +0300

    unittest.mock — mock object library

--- Day_023/testjoke.py
+++ Day_023/testjoke.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 
 import unittest
-from unittest.mock import patch
-from joke import len_joke
+from unittest.mock import patch, MagicMock
+from joke import len_joke, get_joke
 
 
 class TestJoke(unittest.TestCase):
 
-    # we want to mock the get_joke function since it depends on
-    # factors such as internet connection to execute
-
     @patch('joke.get_joke')
     def test_len_joke(self, mock_get_joke):
-        mock_get_joke.return_value = 'one'
-        # we have mocked whatever joke would be returned to 'one' and
-        # its length is 3
-        self.assertEqual(len_joke(), 3)
+        mock_get_joke.return_value = 'Why was the math book sad? Because it had too many problems.'
+        # The length of the joke above is 58
+        self.assertEqual(len_joke(), 60)
+
+    @patch('joke.requests.get')
+    def test_get_joke(self, mock_requests):
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {'setup': 'Why was the math book sad?', 'punchline': 'Because it had too many problems.'}
+
+        mock_requests.return_value = mock_response
+
+        self.assertEqual(get_joke(), 'Why was the math book sad?\nBecause it had too many problems.')
+
 
 if __name__ == '__main__':
     unittest.main()
+
