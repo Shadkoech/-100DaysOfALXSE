7586304b049424372b5e48d2de7d36ab010a6cbc
Shadkoech <shadrackipruto@gmail.com>
2024-04-07 18:57:14 +0300

    Use of memoization on square function

---
+++ Day_025/square.js
@@ -0,0 +1,24 @@
+
+const prevValues = [];
+
+
+function square(n) {
+    if (prevValues[n] != null) {
+        return prevValues[n];
+    }
+
+    let result = 0;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= n; j++) {
+            result += 1;
+        }
+    }
+    prevValues[n] = result;
+    return result;
+}
+// console.log(square(3))
+console.log(square(3000));
+console.log(square(3000));
+console.log(square(3000));
+console.log(square(3000));
+console.log(square(3000));
